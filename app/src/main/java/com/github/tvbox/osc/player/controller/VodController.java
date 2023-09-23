df27a017b9df4d107fa8c027c93b7e26eae28b79
aaccer <103036800+aaccer@users.noreply.github.com>
2023-09-23 22:27:26 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -613,12 +613,21 @@ public class VodController extends BaseController {
         mZimuBtn.setOnLongClickListener(new OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                mSubtitleView.setVisibility(View.GONE);
+                /*mSubtitleView.setVisibility(View.GONE);
                 mSubtitleView.destroy();
                 mSubtitleView.clearSubtitleCache();
                 mSubtitleView.isInternal = false;
                 hideBottom();
+                Toast.makeText(getContext(), "字幕已关闭", Toast.LENGTH_SHORT).show();*/
+                if(mSubtitleView.getVisibility() == View.VISIBLE){
+                mSubtitleView.setVisibility(View.GONE);
+                hideBottom();
                 Toast.makeText(getContext(), "字幕已关闭", Toast.LENGTH_SHORT).show();
+                }else{
+                mSubtitleView.setVisibility(View.VISIBLE);
+                hideBottom();
+                Toast.makeText(getContext(), "字幕已开启", Toast.LENGTH_SHORT).show();
+                }
                 return true;
             }
         });
