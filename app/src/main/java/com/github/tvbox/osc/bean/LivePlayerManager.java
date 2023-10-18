0d2ab76472fd69d9418c4c54b4ae9330f095dd16
aaccer <103036800+aaccer@users.noreply.github.com>
2023-10-18 14:50:25 +0800

    Update LivePlayerManager.java

--- app/src/main/java/com/github/tvbox/osc/bean/LivePlayerManager.java
+++ app/src/main/java/com/github/tvbox/osc/bean/LivePlayerManager.java
@@ -22,10 +22,11 @@ public class LivePlayerManager {
 
     public void init(VideoView videoView) {
         try {
-            defaultPlayerConfig.put("pl", Hawk.get(HawkConfig.PLAY_TYPE, 2));//exo
-            defaultPlayerConfig.put("ijk", Hawk.get(HawkConfig.IJK_CODEC, "软解码"));
+            defaultPlayerConfig.put("pl", Hawk.get(HawkConfig.PLAY_TYPE, 1));//ijk
+            defaultPlayerConfig.put("ijk", Hawk.get(HawkConfig.IJK_CODEC, "硬解码"));
             defaultPlayerConfig.put("pr", Hawk.get(HawkConfig.PLAY_RENDER, 1));
-            defaultPlayerConfig.put("sc", Hawk.get(HawkConfig.PLAY_SCALE, 1));  //16:9
+            //defaultPlayerConfig.put("sc", Hawk.get(HawkConfig.PLAY_SCALE, 1));  //16:9
+            defaultPlayerConfig.put("sc", 1);  //16:9
         } catch (JSONException e) {
             e.printStackTrace();
         }
