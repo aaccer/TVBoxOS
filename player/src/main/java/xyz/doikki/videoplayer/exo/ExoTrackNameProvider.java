3246554c9f6170863fbc2066f1b8d02b3d93a63c
aaccer <103036800+aaccer@users.noreply.github.com>
2023-11-19 18:32:29 +0800

    Update ExoTrackNameProvider.java

--- player/src/main/java/xyz/doikki/videoplayer/exo/ExoTrackNameProvider.java
+++ player/src/main/java/xyz/doikki/videoplayer/exo/ExoTrackNameProvider.java
@@ -4,7 +4,7 @@ import android.content.res.Resources;
 import android.text.TextUtils;
 import androidx.annotation.Nullable;
 
-import com.github.tvbox.osc.R;
+import com.github.tvbox.osc.player.R;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.Format;
 import com.google.android.exoplayer2.util.Assertions;
