c5ac0c71a0a8b0221d69692ec35780040a78fa38
aaccer <103036800+aaccer@users.noreply.github.com>
2023-11-20 17:57:21 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -261,10 +261,11 @@ public class PlayActivity extends BaseActivity {
         SubtitleDialog subtitleDialog = new SubtitleDialog(PlayActivity.this);
         //int playerType = mVodPlayerCfg.getInt("pl");
         //if (mController.mSubtitleView.hasInternal && playerType == 1) {
-            //subtitleDialog.selectInternal.setVisibility(View.VISIBLE);
-        //} else {
-            //subtitleDialog.selectInternal.setVisibility(View.GONE);
-        //}
+        if (mController.mSubtitleView.hasInternal && playerType == 1) {
+            subtitleDialog.selectInternal.setVisibility(View.VISIBLE);
+        } else {
+            subtitleDialog.selectInternal.setVisibility(View.GONE);
+        }
         subtitleDialog.setSubtitleViewListener(new SubtitleDialog.SubtitleViewListener() {
             @Override
             public void setTextSize(int size) {
