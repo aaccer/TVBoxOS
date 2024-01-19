72ff40c845fc155cdc02ac4b234319438b407c88
aaccer <103036800+aaccer@users.noreply.github.com>
2024-01-19 20:29:30 +0800

    Update TrackInfoBean.java

--- app/src/main/java/com/github/tvbox/osc/player/TrackInfoBean.java
+++ app/src/main/java/com/github/tvbox/osc/player/TrackInfoBean.java
@@ -1,14 +1,14 @@
 package com.github.tvbox.osc.player;
 
 public class TrackInfoBean {
-    //public int trackId;
+    public String name;
+    public String language;
+    public int trackId;
+    public boolean selected;
+
     //渲染器ID（exo）
     public int renderId;
     //分组ID（exo）
     public int trackGroupId;
 
-    public String name;
-    public String language;
-    public int index;
-    public boolean selected;
 }
