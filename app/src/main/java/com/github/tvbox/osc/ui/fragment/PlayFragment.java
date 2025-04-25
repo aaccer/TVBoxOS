8775c99d1fb070283fbaf010f1d767c201eac0d4
aaccer <103036800+aaccer@users.noreply.github.com>
2025-04-25 23:36:48 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -570,7 +570,8 @@ public class PlayFragment extends BaseLazyFragment {
     };
 
     void playUrl(String url, HashMap<String, String> headers) {
-        if (!url.contains("://127.0.0.1/") && !url.contains(".m3u8")) {
+         if(!url.startsWith("data:application"))EventBus.getDefault().post(new RefreshEvent(RefreshEvent.TYPE_REFRESH, url));//更新播放地址
+         if (!url.contains("://127.0.0.1/") && !url.contains(".m3u8")) {
             startPlayUrl(url, headers);
             return;
         }
